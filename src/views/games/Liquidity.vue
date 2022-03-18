8af1173672d0367477c131b895dc026d0ea2b3ff
lothar0x <lothar0x@protonmail.com>
2022-03-18 20:08:35 -0300

    Update texts and videos

--- src/views/games/Liquidity.vue
+++ src/views/games/Liquidity.vue
@@ -18,40 +18,30 @@ export default {
       videos: [
         {
           index: "1",
-          title: "Tokens, Guardian and Bridge",
-          videoUrl: "https://www.youtube.com/embed/5DwpmNWrEZc",
+          title: "Token Acquisition",
+          videoUrl: "",
           texts: [
-            "The first step to conquering anything in the medieval metaverse is purchasing the game's utility tokens. With them, you can acquire soldiers, packs, and also pay for services. That is, utility tokens do have a lot of utility.",
-            "Then, it is necessary to ask for help from Otto, a guardian that will facilitate your journey in the game. Once that's done, bring your tokens to the medieval metaverse through the APWars Bridge. It's quite a journey!",
+            "The first step to participate in medieval metaverse tokenomics is to purchase the game's utility tokens. That is, to acquire the main operating token, which is wGOLD.",
+            "Then it is necessary to ask for help from Otto, a guardian who will facilitate your journey in the game. Once that's done, bring your tokens to the medieval metaverse through the APWars Bridge. It's quite a trip!",
             "Watch the video to understand how to do each simple step.",
           ],
         },
         {
           index: "2",
-          title: "Unlock, Play and Recharge",
-          videoUrl: "https://www.youtube.com/embed/5DwpmNWrEZc",
+          title: "Loyalty Program ",
+          videoUrl: "https://www.youtube.com/embed/Z9rt2rb67Mo",
           texts: [
-            "Now it's time for action! To start playing, you need to unlock a Soldier. Once you unlock your soldier, you can show off your gameplay skills in TMJ, the most monstrous beat 'em up in the world!",
-            "After having fun, you'll need to recharge your courage bar to maintain your frequency. Keeping the courage bar full and using refills guarantees more games and, consequently, more rewards.",
+            "For this merchant participation to be good for you, you must participate in the Loyalty Program. Which means that you must provide liquidity. ",
+            "The pair for liquidity provision must be in wGOLD/BUSD.",
             "Watch the video to understand how to do each simple step.",
           ],
         },
         {
           index: "3",
-          title: "War SCARS and Rewards",
-          videoUrl: "https://www.youtube.com/embed/5DwpmNWrEZc",
+          title: "",
+          videoUrl: "",
           texts: [
-            "Playing TMJ allows you to earn many rewards. By playing, you conquer War SCARS!",
-            "And in the Loyalty Shop, you can use your War SCARS to redeem game items. You can trade these game items or use them right away.",
-            "Watch the video to understand how to do each simple step.",
-          ],
-        },
-        {
-          index: "Bonus",
-          title: "Join the Battle",
-          videoUrl: "https://www.youtube.com/embed/5DwpmNWrEZc",
-          texts: [
-            "In addition to giving you War SCARS and allowing you to be a powerful warrior, the mini-game prepares you for wars. That is, it is through TMJ that you evolve to battle well equipped.",
+            "By joining the Loyalty Program, you will earn points (War SCARS) that can be exchanged for game items. These game items can be sold in the Black Market later. Good sales, merchant!",
             "Watch the video to understand how to do each simple step.",
           ],
         },
